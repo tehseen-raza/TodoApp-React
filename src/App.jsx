d4f98bcd351f43f7c3b380a7273c7741688b3306
tehseen-raza <95290584+tehseen-raza@users.noreply.github.com>
2022-08-02 15:30:16 +0500

    Todo App in React

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import './App.css'
+
+const getLocalItems = () => {
+  let tasks = localStorage.getItem('Tasks List')
+  if (tasks) {
+    return JSON.parse(tasks)
+  }
+  else {
+    return [];
+  }
+}
+
+function App() {
+  const [text, setText] = useState('');
+  const [task, setTask] = useState(getLocalItems());
+
+  const changeTextHandler = (e) => {
+    setText(e.target.value)
+  }
+
+  const formSubmitted = (e) => {
+    e.preventDefault();
+    text ? setTask([...task, text]) : alert('Please Add Your First Todo')
+    setText('')
+  }
+
+  const removeTask = (a) => {
+    const finalData = task.filter((curEle, index) => {
+      return index !== a;
+    })
+    setTask(finalData)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('Tasks List', JSON.stringify(task))
+  }, [task])
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center align-items-center main-row">
+        <div className="col shadow main-col bg-white">
+          <div className="row bg-dark text-white">
+            <div className="col p-2">
+              <h4 className='text-center'>Todo App Using React JS</h4>
+            </div>
+          </div>
+
+          <form onSubmit={formSubmitted}>
+            <div className="row justify-content-between text-white p-2">
+              <div className="form-group flex-fill mb-2 col-9">
+
+                <input id="todo-input" placeholder="Please enter what you want to-do" type="text" className="form-control"
+                  value={text} onChange={changeTextHandler} />
+
+              </div>
+              <button type="submit" className="btn btn-dark mb-2 ml-2 col-3">Add todo</button>
+            </div>
+          </form>
+
+          <div className="container">
+            <div className="row">
+              {
+                task.map((val, ind) => {
+                  return (
+                    <>
+                      <div className="col-6 my-2 my-auto">{val}</div>
+                      <div className="col-6 my-2 text-end">
+                        <button type="button" onClick={() => removeTask(ind)} className="btn btn-danger">
+                          Delete
+                        </button>
+                      </div>
+                      <hr />
+                    </>
+                  )
+                })
+              }
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
